11305fc4a7abe52fb3e6a01a4187b1c3fd800852
JoaoPedroPPS <84422500+JoaoPedroPPS@users.noreply.github.com>
2022-03-25 20:48:17 -0300

    Correção círculo

--- src/geometria/Circulo.java
+++ src/geometria/Circulo.java
@@ -1,18 +1,18 @@
 package geometria;
 
 public class Circulo {
-	private double raio;
-	private double pi = 3.1416;
-	
-	public void setRaio(double raio){
-		if(raio > 0){
-			this.raio = raio;
-		}
-	}
-	public double calculoArea() {
-		double area;
-		area = pi * raio * raio;
-		return area;
-	}
-	
+        private double raio;
+        private double pi = 3.1416;
+        
+        public void setRaio(double raio){
+                if(raio > 0){
+                        this.raio = raio;
+                }
+        }
+        public double calculoArea() {
+                double area;
+                area = pi * raio * raio;
+                return area;
+        }
+        
 }
