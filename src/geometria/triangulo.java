78f0717f3cbada78d4f466dbebc645fce63e698b
JoaoPedroPPS <84422500+JoaoPedroPPS@users.noreply.github.com>
2022-03-25 20:45:32 -0300

    Correção triângulo

--- src/geometria/triangulo.java
+++ src/geometria/triangulo.java
@@ -1,42 +1,33 @@
 package geometria;
 
 public class triangulo {
-	private double base;
-	private double altura;
-	private double meio;
-	
-	public void setLados(double base, double altura) {
-		if(base > 0 && altura >0) {
-			this.base =  base;
-			this.altura = altura;
-			
-			
-		}
-		meio = base / 2;
-		
-	}
-	public float calculoArea() {
-		float area;
-		area = (float) ((base * altura ) / 2);
-		return area;
-		
-	}
-	public void desenho(){
-		
-		for(int i=0;i<base+1;i++){
-			for(int j=0;j<i;j++){
-				System.out.print(". ");
-			   }
-		System.out.println("");
+        private double base;
+        private double altura;
+        
+        public void setLados(double base, double altura) {
+                if(base > 0 && altura >0) {
+                        this.base =  base;
+                        this.altura = altura;                
+                }
+                
+        }
+        public float calculoArea() {
+                float area;
+                area = (float) ((base * altura ) / 2);
+                return area;
+                
+        }
+        public void desenho(){
+                
+                for(int i=0;i<base+1;i++){
+                        for(int j=0;j<i;j++){
+                                System.out.print(". ");
+                           }
+                System.out.println("");
 }
-        
-        
-          
+        
+        
+          
 }
-        
-        
-          }
-}
-
-	}
+        
 }
